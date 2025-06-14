b8884df44ea85a59c9fad6de15621a0cfc27734d
hafiz1379 <hafizrasa1379@gmail.com>
2025-06-14 16:46:32 +0430

    Add static file serving and client rendering to app.js

--- api/app.js
+++ api/app.js
@@ -9,6 +9,13 @@ import cookieParser from "cookie-parser";
 import orderRoutes from "./routes/orderRoutes.js";
 import paymentRoutes from "./routes/paymentRoute.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve the __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);  
+const __dirname = path.dirname(__filename);
+
 // Hanlde the uncaught exception errors
 process.on("uncaughtException", (err) => {
   console.log(`ERROR: ${err}`);
@@ -34,6 +41,14 @@ app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", paymentRoutes);
+
+// Use the client app
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Render client app for any path 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 // Middleware to handle errors
 app.use(errorMiddleware);
