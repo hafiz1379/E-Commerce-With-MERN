56607273f6760214c37166f879645de8098a0550
hafiz1379 <hafizrasa1379@gmail.com>
2025-06-14 14:24:29 +0430

    Update API proxy target in vite.config.js to use localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_TARGET,
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
