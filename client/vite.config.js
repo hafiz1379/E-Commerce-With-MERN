4c1119d0db0761424e8039e60c4e89e49a930e87
hafiz1379 <hafizrasa1379@gmail.com>
2025-06-15 10:43:00 +0430

    Refactor Vite configuration to remove unnecessary define block and enhance server proxy settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
