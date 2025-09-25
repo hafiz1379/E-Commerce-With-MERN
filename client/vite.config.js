b7a3ee4b98d84aa61857709bfbfc4f61c588a443
hafiz1379 <hafizrasa1379@gmail.com>
2025-09-25 16:11:54 +0430

    add env var into vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-// Load environment variables
-dotenv.config();
+export default ({ mode }) => {
+  // Load env file based on `mode` (development or production)
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8000",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
